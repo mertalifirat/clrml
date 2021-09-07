7e785d76c3a37e9d4d154f864c923b862e41c42c
mertalifirat <53237645+mertalifirat@users.noreply.github.com>
2021-09-07 09:33:45 +0300

    docker olmadı

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 from clearml import Task
 task = Task.init(project_name='NewWworks', task_name='Say Hello')
-os.system("docker export 0b82f70087a6 > our_image.tar")
 print("blablalbalallasl")
 
